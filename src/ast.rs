8cc25dedb70281a9cd3b0011e807e5889709507c
emanon001 <emanon001@gmail.com>
2020-07-31 22:55:04 +0900

    add parser test

--- src/ast.rs
+++ src/ast.rs
@@ -2,19 +2,19 @@
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Program {
-    statements: Vec<Statement>,
+    pub statements: Vec<Statement>,
 }
 
 // Statement
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Statement {
     Let { ident: Expression, expr: Expression },
 }
 
 // Expression
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Expression {
     Identifier(String),
 }
--- src/parser.rs
+++ src/parser.rs
@@ -25,7 +25,42 @@ impl Parser {
         self.peek_token = self.lexer.next();
     }
 
-    pub fn parse_program() -> ast::Program {
+    pub fn parse_program(&mut self) -> ast::Program {
         panic!();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Parser;
+    use crate::ast::{self};
+    use crate::lexer::Lexer;
+
+    #[test]
+    fn let_statements() {
+        let input = r#"
+        let x = 5;
+        let y = 10;
+        let foobar = 838383;
+        "#
+        .into();
+        let lexer = Lexer::new(input);
+        let mut parser = Parser::new(lexer);
+        let program = parser.parse_program(); // panic
+        let expected_names = vec!["x", "y", "foobar"];
+        for i in 0..expected_names.len() {
+            let s = &program.statements[i];
+            test_let_statement(s, expected_names[i]);
+        }
+    }
+
+    fn test_let_statement(s: &ast::Statement, name: &str) {
+        match s {
+            ast::Statement::Let { ident, .. } => match ident {
+                ast::Expression::Identifier(s) => {
+                    assert_eq!(s, name, "identifier not {}. got={}", name, s);
+                }
+            },
+        };
     }
 }
