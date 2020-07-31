de68103bb2f2f31d68e322f7718bcd6e6c05366c
emanon001 <emanon001@gmail.com>
2020-08-01 04:55:58 +0900

    impl `fmt::Display` in `ast::*`

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,19 @@
+use std::fmt::{self};
+
 // Program
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Program {
     pub statements: Vec<Statement>,
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for s in &self.statements {
+            write!(f, "{}\n", s)?
+        }
+        Ok(())
+    }
 }
 
 // Statement
@@ -14,6 +25,16 @@ pub enum Statement {
     Expression(Expression),
 }
 
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Statement::Let { ident, expr } => write!(f, "let {} = {};", ident, expr),
+            Statement::Return(expr) => write!(f, "return {};", expr),
+            Statement::Expression(expr) => write!(f, "{};", expr),
+        }
+    }
+}
+
 // Expression
 
 #[derive(Debug, PartialEq, Eq, Clone)]
@@ -21,5 +42,19 @@ pub enum Expression {
     Identifier(Identifier),
 }
 
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Expression::Identifier(id) => write!(f, "{}", id),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Identifier(pub String);
+
+impl fmt::Display for Identifier {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
