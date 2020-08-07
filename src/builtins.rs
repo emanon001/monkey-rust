2ea2c51e297da85499e8d08fafc97cc1768f6fa4
emanon001 <emanon001@gmail.com>
2020-08-07 18:24:15 +0900

    add `builtins::first`

--- src/builtins.rs
+++ src/builtins.rs
@@ -9,6 +9,7 @@ lazy_static! {
     static ref FUNCTION_MAP: HashMap<&'static str, BuiltinFunction> = {
         let mut map = HashMap::new();
         map.insert("len", len as BuiltinFunction);
+        map.insert("first", first as BuiltinFunction);
         map
     };
 }
@@ -30,6 +31,24 @@ fn len(args: Vec<Object>) -> Object {
     match &args[0] {
         Object::String(s) => Object::Integer(s.len() as i64),
         Object::Array(array) => Object::Integer(array.len() as i64),
+        o => Object::Error(format!("argument to `len` not supported, got `{}`", o)),
+    }
+}
+
+fn first(args: Vec<Object>) -> Object {
+    // len(o: String | Array) -> Integer
+    if args.len() != 1 {
+        return Object::Error(format!(
+            "wrong number of arguments. got={}, want=1",
+            args.len()
+        ));
+    }
+
+    match &args[0] {
+        Object::Array(array) => match array.first() {
+            Some(f) => f.clone(),
+            None => Object::Null,
+        },
         o => Object::Error(format!("argument to `len` not supported, got `{}`", o)),
     }
 }
@@ -58,6 +77,22 @@ mod tests {
         }
     }
 
+    #[test]
+    fn test_first() {
+        // args, expected
+        let tests = vec![
+            (vec![new_array(Vec::new())], new_null()),
+            (vec![new_array(vec![new_string("a")])], new_string("a")),
+            (
+                vec![new_array(vec![new_integer(2), new_integer(4)])],
+                new_integer(2),
+            ),
+        ];
+        for (args, expected) in tests {
+            assert_eq!(first(args), expected);
+        }
+    }
+
     // helpers
 
     fn new_string(s: &str) -> Object {
@@ -70,5 +105,9 @@ mod tests {
 
     fn new_array(a: Vec<Object>) -> Object {
         Object::Array(a)
+    }
+
+    fn new_null() -> Object {
+        Object::Null
     }
 }
