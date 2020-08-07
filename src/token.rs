3ee0af3b728172488f4dbf81681a0df5bd29e5aa
emanon001 <emanon001@gmail.com>
2020-08-07 15:16:31 +0900

    sort

--- src/token.rs
+++ src/token.rs
@@ -1,30 +1,30 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
-    Assign,
-    Asterisk,
-    Bang,
-    Comma,
-    Else,
-    Eq,
-    False,
-    Function,
-    GT,
-    Identifier(String),
-    If,
+    Int(String),        // 123
+    True,               // true
+    False,              // false
+    String(String),     // "foobar"
+    Identifier(String), // foo, foobar
+    Assign,             // =
+    Bang,               // !
+    Plus,               // +
+    Minus,              // -
+    Asterisk,           // *
+    Slash,              // /
+    Eq,                 // ==
+    NotEq,              // !=
+    Function,           // fn
+    GT,                 // >=
+    LT,                 // <=
+    If,                 // if
+    Else,               // else
+    Let,                // let
+    Return,             // return
+    LParen,             // (
+    RParen,             // )
+    LBrace,             // {
+    RBrace,             // }
+    Comma,              // ,
+    Semicolon,          // ;
     Illegal(String),
-    Int(String),
-    LBrace,
-    LParen,
-    LT,
-    Let,
-    Minus,
-    NotEq,
-    Plus,
-    RBrace,
-    RParen,
-    Return,
-    Semicolon,
-    Slash,
-    String(String),
-    True,
 }
